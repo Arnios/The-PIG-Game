b005424b2e2098fe05ab8c25ad11b491edf5e88f
unknown <arnabaroy@outlook.com>
2018-03-10 09:25:42 +0530

    + Scripted 'Hold' Button

--- app.js
+++ app.js
@@ -43,3 +43,27 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         
     }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+
+    // 1. Stores the round score to the player's total score and show it in UI
+
+    score[activePlayer] = score[activePlayer] + roundScore;
+    document.getElementById('score-' + activePlayer).textContent = score[activePlayer];
+
+    // 2. Reset round score and show it in UI
+
+    roundScore = 0;
+    document.getElementById('current-' + activePlayer).textContent = roundScore;
+
+    // 3. Change active player and shift the active player panel
+
+    if(activePlayer === 0)
+        activePlayer = 1;
+    else if(activePlayer === 1)
+        activePlayer = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+});
